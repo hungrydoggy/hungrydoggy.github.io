3a03ff2001e3ed8459223075ef522d55c3edf72a
JongHeon_Kim <jhoney510@gmail.com>
2019-05-18 09:16:27 +0900

    Add controller-item-detail

--- src/common/lecture_info.js
+++ src/common/lecture_info.js
@@ -106,6 +106,10 @@ export default {
     name: 'Widget & State/메인 위젯 구성하기/[미션] 장바구니',
     md_source_path: require('../md/flutter/Fast Campus/2019-05-11/02.03.07.cart.md'),
   },
+  '/flutter/Fast-Campus/2019-05-11/controller/text-editting-controller' : {
+    name: 'Widget & State/컨트롤러/TextEdittingController',
+    md_source_path: require('../md/flutter/Fast Campus/2019-05-11/02.04.00.text-editting-controller.md'),
+  },
   '/flutter/Fast-Campus/2019-05-11/second-homework' : {
     name: 'Widget & State/Widget & State 숙제',
     md_source_path: require('../md/flutter/Fast Campus/2019-05-11/02.99.homework.md'),
